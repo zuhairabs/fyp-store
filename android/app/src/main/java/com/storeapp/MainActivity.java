518ad022a820a5f6ec595232c019a39237f2784f
Suryansh Sugandhi <suryansh71199@gmail.com>
2020-10-08 12:55:35 +0530

    add: firebase messaging and notifications

--- android/app/src/main/java/com/storeapp/MainActivity.java
+++ android/app/src/main/java/com/storeapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.storeapp;
+package com.shopout.store;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "storeapp";
+    return "shopout.store";
   }
 }
--- android/app/src/main/java/com/storeapp/MainApplication.java
+++ android/app/src/main/java/com/storeapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.storeapp;
+package com.shopout.store;
 
 import android.app.Application;
 import android.content.Context;
@@ -65,7 +65,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.storeapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.shopout.store.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
