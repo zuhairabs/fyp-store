820e030367bc4b73d49ab6516c7d3e10eb39345c
Zuhair Abbas <zuhairabs@gmail.com>
2022-02-18 16:35:03 +0530

    Deploy

--- api/constants.js
+++ api/constants.js
@@ -1,5 +1,6 @@
-export const BASE_URI = 'https://api.shopout.co.in';
+// export const BASE_URI = 'https://api.shopout.co.in';
 // export const BASE_URI = 'https://shopout-demo.herokuapp.com';
+export const BASE_URI = 'https://shopout-project.herokuapp.com';
 export const URI = `${BASE_URI}/store`;
 export const RTC_URI = `${BASE_URI}/rtc-video`;
 
